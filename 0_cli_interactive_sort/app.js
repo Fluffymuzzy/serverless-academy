2d3600c8f4d74f938fd8077ea38361036d8ce94c
Fluffymuzzy <77502156+Fluffymuzzy@users.noreply.github.com>
2023-02-07 16:36:56 +0200

    take input, ask the user how they want to sort it, and then sort :arrow_backward:

--- 0_cli_interactive_sort/app.js
+++ 0_cli_interactive_sort/app.js
@@ -33,16 +33,43 @@ const getUniqueValues = (arr) => {
   return [...new Set(arr.map((x) => (isNaN(x) ? x : +x)))];
 };
 
+// our response
 let inputArray = [];
+
 const sort = (input) => {
   rl.question(
-    "How would you like to sort the input?",
-    "1. let's sort words",
+    "How would you like to sort the input?\n" +
+      "1. Sort words alphabetically\n" +
+      "2. Show numbers from lesser to greater\n" +
+      "3. Show numbers from bigger to smaller\n" +
+      "4. Display words in ascending order by number of letters in the word\n" +
+      "5. Show only unique words\n" +
+      "6. Display only unique values from the set of words and numbers entered by the user\n" +
+      "7. To exit the program, enter 'exit'\n",
     (answer) => {
       switch (answer) {
         case "1":
           console.log(sortWords(input));
           break;
+        case "2":
+          console.log(sortNumbersAsc(input));
+        case "3":
+          console.log(sortNumbersDesc(input));
+          break;
+        case "4":
+          console.log(sortWordsByLength(input));
+          break;
+        case "5":
+          console.log(getUniqueWords(input));
+          break;
+        case "6":
+          console.log(getUniqueValues(input));
+          break;
+        case "exit":
+          rl.close;
+          break;
+        default:
+          console.log("Invalid option");
       }
     }
   );
