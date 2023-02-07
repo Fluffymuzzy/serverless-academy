321ac3a48a94545008380a1a776f392b622e7030
Fluffymuzzy <77502156+Fluffymuzzy@users.noreply.github.com>
2023-02-07 16:19:24 +0200

    adding a sort function :+1:

--- 0_cli_interactive_sort/app.js
+++ 0_cli_interactive_sort/app.js
@@ -3,7 +3,52 @@ const rl = require("readline").createInterface({
   output: process.stdout,
 });
 
-rl.question("let's start here.", (answer) => {
-  console.log(`${answer}`);
-  rl.close();
+// Sort words alphabetically
+const sortWords = (arr) => {
+  return arr.sort((a, b) => a.localeCompare(b));
+};
+
+// Show numbers from lesser to greater
+const sortNumbersAsc = (arr) => {
+  return arr.sort((a, b) => a - b);
+};
+
+// Show numbers from bigger to smaller
+const sortNumbersDesc = (arr) => {
+  return arr.sort((a, b) => b - a);
+};
+
+// Display words in ascending order by number of letters in the word
+const sortWordsByLength = (arr) => {
+  return arr.sort((a, b) => a.length - b.length);
+};
+
+// Show only unique words
+const getUniqueWords = (arr) => {
+  return [...new Set(arr)];
+};
+
+// Display only unique values from the set of words and numbers entered by the user
+const getUniqueValues = (arr) => {
+  return [...new Set(arr.map((x) => (isNaN(x) ? x : +x)))];
+};
+
+let inputArray = [];
+const sort = (input) => {
+  rl.question(
+    "How would you like to sort the input?",
+    "1. let's sort words",
+    (answer) => {
+      switch (answer) {
+        case "1":
+          console.log(sortWords(input));
+          break;
+      }
+    }
+  );
+};
+
+rl.question("enter a few words...", (answer) => {
+  inputArray = answer.split(" ");
+  sort(inputArray);
 });
