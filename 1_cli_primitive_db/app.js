b75eafb123f18d06005e35a23012350cf6fec168
Fluffymuzzy <77502156+Fluffymuzzy@users.noreply.github.com>
2023-02-09 20:58:02 +0200

    Add a function that looks for a user in the database :abc:

--- 1_cli_primitive_db/app.js
+++ 1_cli_primitive_db/app.js
@@ -3,7 +3,7 @@ import fs from "fs";
 
 let db = [];
 //  A function that is asking the user to input information.
-const addUser = (user) => {
+const addUser = () => {
   inquirer
     .prompt([
       {
@@ -16,11 +16,13 @@ const addUser = (user) => {
         name: "gender",
         message: "Select gender:",
         choices: ["Male", "Female"],
+        when: ({ name }) => name,
       },
       {
         type: "input",
         name: "age",
         message: "Enter age:",
+        when: ({ name }) => name,
       },
     ])
     // Push (P) data from the user's input into the database.
@@ -53,8 +55,54 @@ const addUser = (user) => {
     });
 };
 
+// The function looks up the user in the database.
 const searchUserByName = () => {
-  console.log("sup mate ?");
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Enter user name to search: ",
+      },
+    ])
+    .then(({ name }) => {
+      const foundUser = db.find((user) => user.name === name);
+      if (foundUser) {
+        console.log(`User found: ${JSON.stringify(foundUser)}`);
+        inquirer
+          .prompt([
+            {
+              type: "confirm",
+              name: "searchAgain",
+              message: "Do you want to search for another user?",
+            },
+          ])
+          .then(({ searchAgain }) => {
+            if (searchAgain) {
+              searchUserByName();
+            } else {
+              console.log("Have a nice day!");
+            }
+          });
+      } else {
+        console.log("User not found.");
+        inquirer
+          .prompt([
+            {
+              type: "confirm",
+              name: "searchAgain",
+              message: "Do you want to search for another user?",
+            },
+          ])
+          .then(({ searchAgain }) => {
+            if (searchAgain) {
+              searchUserByName();
+            } else {
+              console.log("Dopobachennya!");
+            }
+          });
+      }
+    });
 };
 
 addUser();
