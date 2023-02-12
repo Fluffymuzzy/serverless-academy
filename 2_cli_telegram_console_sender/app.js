3b10e0c4318d40afb65187091c13f99d73fa6fd6
Fluffymuzzy <77502156+Fluffymuzzy@users.noreply.github.com>
2023-02-12 13:35:48 +0200

    Sending a message to the Telegram bot :accept:

--- 2_cli_telegram_console_sender/app.js
+++ 2_cli_telegram_console_sender/app.js
@@ -3,8 +3,38 @@ const commander = require("commander");
 const token = "5507382020:AAH0D-SCqeZew2HSviewaxAnajbm7BuhOxA";
 const bot = new TelegramBot(token, { polling: true });
 
-bot.onText(/\/start/, (msg) => {
-  chatId = msg.chat.id;
-  console.log(`Chat ID: ${chatId}`);
-  process.exit();
-});
+// Get Chat ID
+function getChatId() {
+  bot.onText(/\/start/, (msg) => {
+    chatId = msg.chat.id;
+    console.log(`Chat ID: ${chatId}`);
+    process.exit();
+  });
+}
+
+commander
+  .command("get-chat-id")
+  .alias("g")
+  .description("Get the chat ID for the Telegram bot")
+  .action(() => {
+    getChatId();
+  });
+
+//  Sending a message to the Telegram bot.
+commander
+  .command("send-message <message>")
+  .alias("m")
+  .description("Send a message to the Telegram bot")
+  .option("-c, --chat-id <value>", "Chat ID of the recipient")
+  .action((message, options) => {
+    bot
+      .sendMessage(options.chatId, message)
+      .then(() => {
+        console.log("Message sent successfully!");
+        process.exit();
+      })
+      .catch((err) => {
+        console.error(err);
+        process.exit();
+      });
+  });
