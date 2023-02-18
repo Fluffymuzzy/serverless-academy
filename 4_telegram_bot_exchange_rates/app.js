fe4b43804b2770ab42ee53f2df4a30507f6a2332
Fluffymuzzy <77502156+Fluffymuzzy@users.noreply.github.com>
2023-02-18 17:03:25 +0200

    added functions that get the exchange rate from Mono and Privat API

--- 4_telegram_bot_exchange_rates/app.js
+++ 4_telegram_bot_exchange_rates/app.js
@@ -3,7 +3,7 @@ const TelegramBot = require("node-telegram-bot-api");
 const axios = require("axios");
 
 // Initialize the Telegram bot with your bot token
-const TOKEN = '6201228619:AAEjpwLf4ymsP2bMXlm_ueUUFb7QJWIf08I';
+const TOKEN = "6201228619:AAEjpwLf4ymsP2bMXlm_ueUUFb7QJWIf08I";
 const bot = new TelegramBot(TOKEN, { polling: true });
 
 // Function to handle the /start command
@@ -19,3 +19,41 @@ bot.onText(/\/start/, async (msg) => {
   };
   bot.sendMessage(chatId, message, options);
 });
+
+// Function to get the exchange rate from PrivatBank API
+async function getPrivatExchangeRate() {
+  try {
+    const response = await axios.get(
+      "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5"
+    );
+    const usdRate = response.data.find((rate) => rate.ccy === "USD").buy;
+    const eurRate = response.data.find((rate) => rate.ccy === "EUR").buy;
+    return { usd: usdRate, eur: eurRate };
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+// Function to get the exchange rate from Monobank API
+async function getMonoExchangeRate() {
+  try {
+    const response = await axios.get("https://api.monobank.ua/bank/currency");
+    if (!Array.isArray(response.data)) {
+      throw new Error("Invalid Monobank API response");
+    }
+    const usdRateObj = response.data.find(
+      (rate) => rate.currencyCodeA === 840 && rate.currencyCodeB === 980
+    );
+    const eurRateObj = response.data.find(
+      (rate) => rate.currencyCodeA === 978 && rate.currencyCodeB === 980
+    );
+    if (!usdRateObj || !eurRateObj) {
+      throw new Error("Missing exchange rate data in Monobank API response");
+    }
+    return { usd: usdRateObj.rateBuy, eur: eurRateObj.rateBuy };
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
