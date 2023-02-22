f10d525ebed4b01efcfc3a75360d8b56866b2bae
Fluffymuzzy <77502156+Fluffymuzzy@users.noreply.github.com>
2023-02-22 18:06:39 +0200

    that's it :airplane:

--- 6._instagram_giveaway/app.js
+++ 6._instagram_giveaway/app.js
@@ -52,9 +52,29 @@ async function existInAllFiles() {
   return allCount;
 }
 
+async function existInAtleastTen() {
+  const counts = {};
+  for (let i = 0; i <= 19; i++) {
+    const filename = `words/out${i}.txt`;
+    const fileCounts = await countUsernamesInFile(filename);
+    for (const [username, count] of Object.entries(fileCounts)) {
+      counts[username] = (counts[username] || 0) + 1;
+    }
+  }
+  const atleastTenCount = Object.keys(counts).filter(
+    (username) => counts[username] >= 10
+  ).length;
+  console.log(
+    `Found ${atleastTenCount} usernames that exist in at least 10 files.`
+  );
+  return atleastTenCount;
+}
+
 async function main() {
-  const uniqueCount = await uniqueValues();
-  const allCount = await existInAllFiles();
+  await uniqueValues();
+  await existInAllFiles();
+  await existInAtleastTen();
+  console.log("That's it.");
 }
 
 main();
